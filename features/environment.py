3b864f4cac83c1da343ab77ae71ef08b0399e1b0
herickmotauk@gmail.com <OSLO.norway01>
2019-11-14 15:27:55 -0300

    moving modules

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,11 @@
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
 
 def before_all(context):
-    context.driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--windows-size=1024,768")
+    context.driver = webdriver.Chrome(chrome_options=chrome_options)
 
 
 def after_all(context):
